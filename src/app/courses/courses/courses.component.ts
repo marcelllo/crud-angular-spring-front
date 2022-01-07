1eb4b085921babe19ab9e40c9a71f08a780a3c7d
Marcelo Santos <marcello.santos@gmail.com>
2022-01-07 20:34:43 -0300

    Inclusão do diálogo de tratamento de erro

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, delay, Observable, of } from 'rxjs';
 
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-
-
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   constructor(
-    private courseService: CoursesService
+    private courseService: CoursesService,
+    private dialog: MatDialog
   ) {
-    this.courses$ = courseService.list();
+    this.courses$ = this.courseService.list().pipe(
+      delay(2000),
+      catchError(e => {
+        this.onError('Erro ao carregar cursos.');
+        return of([]);
+      })
+    );
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onError(errorMessage: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMessage,
+    });
+  }
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,16 +1,19 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Course } from '../model/course';
+import { Injectable } from '@angular/core';
 import { delay } from 'rxjs';
+
+import { Course } from '../model/course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly API = '/assets/coursess.json';
+  private readonly API = '/assets/courses.json';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
   list() {
      return this.httpClient.get<Course[]>(this.API)
