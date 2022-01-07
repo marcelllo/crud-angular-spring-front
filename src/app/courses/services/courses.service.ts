486e200b2153e6a3e8f57f958f74a74d3bb7eead
Marcelo Santos <marcello.santos@gmail.com>
2022-01-07 19:26:38 -0300

    Conclusão do spinner para a listagem de cursos

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Course } from '../model/course';
+import { delay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = '/assets/coursess.json';
 
   constructor(private httpClient: HttpClient) { }
 
   list() {
-     return this.httpClient.get<Course[]>(this.API);
+     return this.httpClient.get<Course[]>(this.API)
+      .pipe(
+        delay(5000)
+      );
   }
 }
--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
-  exports: [ MatCardModule, MatTableModule, MatToolbarModule ]
+  exports: [
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatToolbarModule
+  ]
 })
 export class AppMaterialModule { }
