8fc14e9d767d5ed9abb9d742fd1dd80fb508b058
Marcelo Santos <marcello.santos@gmail.com>
2023-07-09 14:40:22 -0300

    Inclusão do spinner para quando demorar carregar os dados

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -10,10 +10,10 @@ import { CoursesService } from './../services/courses.service';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService) {
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
   }
 }
--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -8,6 +8,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   declarations: [],
   exports: [
     MatCardModule,
+    MatProgressSpinnerModule,
     MatTableModule,
     MatToolbarModule
   ]
