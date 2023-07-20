dfd9896eb0667f0ab0961bd85228f0a32b35adc4
Marcelo Santos <marcello.santos@gmail.com>
2023-07-20 01:14:42 -0300

    Refactory, criando métodos create e update

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -27,6 +27,19 @@ export class CoursesService {
   }
 
   save(course: Partial<Course>) {
+
+    if (course._id) {
+      return this.update(course);
+    }
+
+    return this.create(course);
+  }
+
+  private create(course: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, course).pipe(first());
+  }
+
+  private update(course: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${course._id}`, course).pipe(first());
   }
 }
