22219bb6eab4bd59e5691dbe98f822e76647c45d
Marcelo Santos <marcello.santos@gmail.com>
2023-10-18 20:41:23 -0300

    Aula 49: Criação dos campos do formulário dinamicamente

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,11 +1,12 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
 import { CoursesService } from '../../services/courses.service';
 import { Course } from '../../model/course';
+import { Lesson } from '../../model/lesson';
 
 @Component({
   selector: 'app-course-form',
@@ -14,11 +15,7 @@ import { Course } from '../../model/course';
 })
 export class CourseFormComponent implements OnInit {
 
-  form = this.fb.group({
-    _id: [''],
-    name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
-    category: ['', [Validators.required]]
-  });
+  form!: FormGroup;
 
   constructor(
     private fb: NonNullableFormBuilder,
@@ -31,10 +28,35 @@ export class CourseFormComponent implements OnInit {
 
   ngOnInit(): void {
     const course: Course = this.route.snapshot.data['course'];
-    this.form.setValue({
-      _id: course._id,
-      name: course.name,
-      category: course.category
+
+    this.form = this.fb.group({
+      _id: [course._id],
+      name: [course.name, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+      category: [course.category, [Validators.required]],
+      lessons: this.fb.array(this.retrieveLessons(course))
+    });
+
+    console.log(this.form)
+    console.log(this.form.value);
+
+  }
+
+  private retrieveLessons(course: Course) {
+    const lessons = [];
+    if (course?.lessons) {
+      course.lessons?.forEach(lesson => lessons.push(this.createLesson(lesson)));
+    } else {
+      lessons.push(this.createLesson());
+    }
+
+    return lessons;
+  }
+
+  private createLesson(lesson: Lesson = {id: '', name: '', youtubeLink: ''}) {
+    return this.fb.group({
+      id: [lesson.id],
+      name: [lesson.name],
+      youtubeLink: [lesson.youtubeLink],
     });
   }
 
